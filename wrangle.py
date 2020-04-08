8c0d5beeb446dd5a0951711f38428a7a4d20df8b
Ravinder Singh <ravinder79@gmail.com>
2020-04-07 21:38:53 -0500

    wrangle data

--- wrangle.py
+++ wrangle.py
@@ -24,13 +24,15 @@ def wrangle_zillow():
     Returns a clean df with four columns:
     customer_id(object), monthly_charges(float), tenure(int), total_charges(float)
     """
-    customers = get_data_from_sql()
-    #customers = pd.read_sql("SELECT customer_id, monthly_charges, tenure, total_charges FROM customers WHERE contract_type_id = 3", env.get_db_url('telco_churn'))
-    customers['total_charges'] = customers['total_charges'].str.strip()
-    customers = customers.replace(r'^\s*$', np.nan, regex=True)
-    customers = customers.dropna()
-    customers['total_charges'] = customers['total_charges'].astype(float)
-    return customers
+    df = get_data_from_sql()
+    df = df.dropna(subset=['lot_size', 'year_built', 'tax_value', 'zip_code'])
+    df.fillna(0, inplace = True)
+    df['fips'] =df.fips.astype(int)
+    df['zip_code'] =df.zip_code.astype(int)
+    df['year_built'] =df.year_built.astype(int)
+    df['lot_size'] =df.lot_size.astype(int)
+    df['pool'] =df.pool.astype(int)
+    return df
     
 
 #print(customers.sort_values(by='total_charges'))
